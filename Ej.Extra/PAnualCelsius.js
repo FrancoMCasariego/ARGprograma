96e294f984997d5799c335a1bff5c3e67ccfab45
FrancoMCasariego <francomartincasariego@gmail.com>
2023-05-27 11:31:07 -0300

    EJ.Extra3

---
+++ Ej.Extra/PAnualCelsius.js
@@ -0,0 +1,15 @@
+let m1=54;
+let m2=67;
+let m3=23;
+let m4=43;
+let m5=12;
+let m6=51;
+let m7=8;
+let m8=28;
+let m9=12;
+let m10=43;
+let m11=56;
+let m12=76;
+let fahrenheitAnual=((m1+m2+m3+m4+m5+m6+m7+m8+m9+m10+m11+m12)/12);
+let celsiusPAnual=(fahrenheitAnual -32)*0.55
+console.log(celsiusPAnual);
