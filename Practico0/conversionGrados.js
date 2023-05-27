a3530845495d0b5a0dc1c6a4a602e8834e906bc7
FrancoMCasariego <134016299+FrancoMCasariego@users.noreply.github.com>
2023-05-27 10:58:27 -0300

    Agrego el archivo conversionGrados.js

--- Practico0/conversionGrados.js
+++ Practico0/conversionGrados.js
@@ -1,9 +1,9 @@
-var fahrenheit=50
-var celsius=(fahrenheit -32)*0.55
-console.log(celsius)
+var fahrenheit=50;
+var celsius=(fahrenheit -32)*0.55;
+console.log(celsius);
 //resultado es 9.9
 
-var fahrenheit=75
-var celsius=(fahrenheit -32)*0.55
-console.log(celsius)
+var fahrenheit=75;
+var celsius=(fahrenheit -32)*0.55;
+console.log(celsius);
 //resultado es 23.65
